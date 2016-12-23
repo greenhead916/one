b2bfabeeec677c0b6baff349933b4f058f1553f2
Kevin Grabow <kevingrabow@Kevins-MacBook-Pro.local>
2016-12-23 12:45:59 -0500

    Another commit

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ var app = express();
 
 
 app.get('/', (req,res) => {
+  res.send('Home')
   console.log("Someone found the home page")
+})
+
+app.get('/about', (req,res) => {
+  res.send('About')
+  console.log("Someone found the about page")
 })
 
 app.listen(port, ()=> {
